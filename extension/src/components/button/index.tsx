a6632c9d6519c0bed6d746e78487bc4874c8b8ff
obiabo <54102389+yhoungdev@users.noreply.github.com>
2025-04-11 11:38:31 +0100

    added minor ui

--- extension/src/components/button/index.tsx
+++ extension/src/components/button/index.tsx
@@ -22,7 +22,7 @@ const Button = ({
   const baseStyles = ' cursor-pointer rounded-lg font-medium transition-colors duration-200';
   
   const variants = {
-    primary: 'bg-red-600 hover:bg-blue-600 text-white',
+    primary: 'bg-red-600 hover:bg-gray-800 text-white',
     secondary: 'bg-gray-200 hover:bg-gray-300 text-gray-800',
     danger: 'bg-red-500 hover:bg-red-600 text-white'
   };
--- extension/src/pages/Popup.tsx
+++ extension/src/pages/Popup.tsx
@@ -11,12 +11,22 @@ export default function() {
     <div className='text-white'>
       <Header/>
       
+
+      <div className='flex items-center flex-col cursor-pointer justify-center mx-auto rounded-md bg-gray-800 p-4 mt-4 w-[95%] h-[200px] '>
+          <h3 className={`text-5xl ${isRecording ? 'animate-pulse' : ''}`}>🌶️</h3>
+          <p className='mt-2 text-gray-400'>
+            {
+              isRecording ? 'Recording in progress...' : 'Click the button below to start'
+            }
+          </p>
+      </div>
+
       <Button 
         variant={isRecording ? 'danger' : 'primary'}
-        className='min-w-full' 
+        className='min-w-full mt-3' 
         onClick={isRecording ? stopCapture : captureMedia}
       >
-        {isRecording ? 'Stop Recording' : 'Start Recording'}
+        {isRecording ? 'Stop Recording' : 'Start Recording 🌶️'}
       </Button>
 
       {isRecording && (
